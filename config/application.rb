14dbd08cc03615140e39a708a0f6dad4f38605ea
Casey Ochieng <caseyochieng46@gmail.com>
2023-03-17 09:33:01 +0300

    completed

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ require "action_view/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Phase4RailsAndActiveRecordReadme
+module Plantsy
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
@@ -35,6 +35,15 @@ module Phase4RailsAndActiveRecordReadme
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
+    config.api_only = true    
+
+    # Adding back cookies and session middleware
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+
+    # Use SameSite=Strict for all cookies to help protect against CSRF
+    # https://owasp.org/www-community/SameSite
+    config.action_dispatch.cookies_same_site_protection = :strict
+
   end
 end
